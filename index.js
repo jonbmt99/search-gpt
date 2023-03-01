d9b6ca9530dfd408acec8ad1782980ccdb5bc635
Truong Nguyen <mrherik@gmail.com>
2023-03-02 00:03:17 +0700

    fix-search

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const OPENAI_API_KEY=''
-const input_file_name = 'input_example.json';
-const output_file_name = 'output_example.json';
+const require = createRequire(import.meta.url); 
+require('dotenv').config();
+const OPENAI_API_KEY = process.env['OPENAI_API_KEY'];
+const input_file_name = process.env['INPUT_FILE_NAME'];
+const output_file_name = process.env['OUTPUT_FILE_NAME'];
+const rows = require(`./data-input/${input_file_name}`);
+const dns = require('dns');
+const XLSX = require("xlsx");
+
 const input_row = parseInt(process.argv.splice(2).join(" "), 10);
+
 
 import { Configuration, OpenAIApi } from "openai";
 import * as fs from "fs";
 import { createRequire } from "module"; 
-const require = createRequire(import.meta.url); 
-const rows = require(`./data-input/${input_file_name}`);
-const dns = require('dns');
+
 
 const openai = new OpenAIApi(
   new Configuration({
@@ -27,6 +32,7 @@ function AIResponse(question) {
 }
 
 async function main() {
+  console.log('total-input: ', rows.length);
   const output = [];
   let i = input_row - 1;
   let retry_count = 1;
@@ -36,30 +42,22 @@ async function main() {
     const company =  row['COMPANY-ENTREPRISE'];
     const city = row['CITY-VILLE'];
     const question = `
-      Format answer is Email: , Website:
+      Format is: Email: , Website:
       Find email and website of company ${company} in ${city}?
       ${sub_question}
       `;
-    console.log('retry_count: ', retry_count);
-
     try {
-      console.log('current row: ', i + 1);
-      console.log('question: ', question);
+      console.log('current row: ', i + 1, 'retry_count', retry_count);
       const completion = await AIResponse(question)
       let answer = completion.data.choices[0].text.trim();
-      console.log('answer: ', answer);
       const answer_array = answer.split(' ');
       const website = answer_array[answer_array.length - 1];
-      console.log('website: ', website);
       const options = {
         family: 6,
         hints: dns.ADDRCONFIG | dns.V4MAPPED,
       };
       options.all = true;
-      console.log('11111');
       const address = await lookupPromise(website);
-      console.log('address: ', address);
-      console.log('2222222');
       if (address) {
         row.result = answer;
         output.push(row);
@@ -76,7 +74,11 @@ async function main() {
             `./data-output/${output_file_name}`,
             JSON.stringify(current_output, null, 2)
           );
-          fs.writeFileSync(`./current-row/current-row-of-${input_file_name}`, i);
+          const worksheet = XLSX.utils.json_to_sheet(current_output);
+          const workbook = XLSX.utils.book_new();
+          XLSX.utils.book_append_sheet(workbook, worksheet, "data");
+          XLSX.writeFile(workbook, `./data-output-excel/${output_file_name.replace('.json', '')}.xlsx`);
+          fs.writeFileSync(`./current-row/current-row-of-${input_file_name}`, `index row done ${i}`);
       } else if (retry_count > 5) {
           retry_count = 1;
           i++;
@@ -92,12 +94,25 @@ async function main() {
             `./data-output/${output_file_name}`,
             JSON.stringify(current_output, null, 2)
           );
-          fs.writeFileSync(`./current-row/current-row-of-${input_file_name}`, i);
+
+          const worksheet = XLSX.utils.json_to_sheet(current_output);
+          const workbook = XLSX.utils.book_new();
+          XLSX.utils.book_append_sheet(workbook, worksheet, "data");
+          XLSX.writeFile(workbook, `./data-output-excel/${output_file_name.replace('.json', '')}.xlsx`);
+
+          fs.writeFileSync(`./current-row/current-row-of-${input_file_name}`, `index row done: ${i}`);
       } else {
-        sub_question += `website of answer don't like ${website} \n`
+        sub_question += `website don't like ${website} \n`
         retry_count ++;
       }
     } catch(e) {
+      if (e && e.response && e.response.status === 401) {
+        console.log('==== OpenAI token is not valid');
+        return;
+      }
+      if (e && e.response && e.response.status === 429) {
+        console.log('==== Too many request')
+      }
       console.log('error: ', e);
     }
   }
